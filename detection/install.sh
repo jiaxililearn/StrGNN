e14ce23b77d3c7df321520936614eac3639a3497
Jiaxi Li <jiaxili0319@gmail.com>
2023-06-05 10:05:36 +1000

    Added gitignore

--- detection/install.sh
+++ detection/install.sh
@@ -10,5 +10,5 @@ pip install --user numpy
 pip install --user scipy
 pip install --user networkx
 pip install --user tqdm
-pip install --user sklearn
+pip install --user scikit-learn
 pip install --user gensim
